36e7aadd6d38630c328dcd28fdeebf4e0dad0f0a
jtompkin <jtompkin-dev@pm.me>
2025-05-27 17:10:54 -0400

    add completion hosts

---
+++ hosts/x86_64-linux/completion/default.nix
@@ -0,0 +1,12 @@
+{
+  networking.hostName = "completion";
+
+  wunkus = {
+    settings = {
+      username = "none";
+      system = "x86_64-linux";
+    };
+  };
+
+  system.stateVersion = "25.05";
+}
---
+++ hosts/x86_64-linux/none@completion/default.nix
@@ -0,0 +1,10 @@
+{
+  wunkus = {
+    settings = {
+      host = "completion";
+      username = "none";
+      system = "x86_64-linux";
+    };
+  };
+  home.stateVersion = "25.05";
+}
