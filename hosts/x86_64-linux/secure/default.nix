bcc5d1d2da03b713297feee971caefc1e2e9c1ce
jtompkin <jtompkin-dev@pm.me>
2025-07-29 19:16:37 -0400

    add kvm hardware config

---
+++ hosts/x86_64-linux/secure/default.nix
@@ -0,0 +1,30 @@
+{
+  networking.hostName = "secure";
+
+  wunkus = {
+    settings = {
+      username = "josh";
+      system = "x86_64-linux";
+    };
+    profiles = {
+      minimal.enable = true;
+      vm.enable = true;
+      ephemeral.enable = true;
+    };
+    disks = {
+      btrfsEncrypt = {
+        enable = true;
+        swapSize = "4G";
+        mainDevice = "/dev/vda";
+      };
+    };
+    hardware."kvm-x86_64-linux".enable = true;
+  };
+
+  time.timeZone = "America/New_York";
+
+  console.useXkbConfig = true;
+  services.xserver.xkb.options = "caps:swapescape";
+
+  system.stateVersion = "25.05";
+}
--- modules/nixos/wunkus/hardware/kvm-x86_64-linux-module.nix
+++ modules/nixos/wunkus/hardware/kvm-x86_64-linux-module.nix
@@ -1,37 +1,36 @@
 {
-  lib,
   config,
+  lib,
+  modulesPath,
   ...
 }:
 let
-  cfg = config.wunkus.hardware."virtualbox-x86_64-linux";
+  cfg = config.wunkus.hardware."kvm-x86_64-linux";
   inherit (lib) mkDefault mkEnableOption mkIf;
 in
 {
   options = {
-    wunkus.hardware."virtualbox-x86_64-linux".enable =
-      mkEnableOption "virtualbox x86_64-linux hardware config";
+    wunkus.hardware."kvm-x86_64-linux".enable = mkEnableOption "kvm x86_64-linux hardware config";
   };
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+  ];
   config = mkIf cfg.enable {
     boot = {
       initrd = {
         availableKernelModules = [
-          "ata_piix"
-          "ohci_pci"
-          "ehci_pci"
           "ahci"
-          "sd_mod"
+          "xhci_pci"
+          "virtio_pci"
           "sr_mod"
+          "virtio_blk"
         ];
         kernelModules = [ "dm-snapshot" ];
       };
-      kernelModules = [ ];
+      kernelModules = [ "kvm-amd" ];
       extraModulePackages = [ ];
     };
 
-    networking.useDHCP = mkDefault true;
-
     nixpkgs.hostPlatform = mkDefault "x86_64-linux";
-    virtualisation.virtualbox.guest.enable = true;
   };
 }
