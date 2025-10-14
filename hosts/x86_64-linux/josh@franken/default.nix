8e1ee3388075c551eeac22d65c7ce15bfa3c7f91
jtompkin <jtompkin-dev@pm.me>
2025-10-14 15:57:38 -0400

    dunno some stuff probs

--- hosts/x86_64-linux/josh@franken/default.nix
+++ hosts/x86_64-linux/josh@franken/default.nix
@@ -11,14 +11,17 @@
     presets = {
       programs = {
         meantToFly.enable = true;
-        go.enable = true;
+        zim.enable = true;
       };
       themes.dark.enable = true;
     };
   };
 
-  programs.oh-my-posh.enable = false;
-  programs.git.signing.key = "8C07A97FC369A5F4FCFAC6F1989246B0B9904782";
+  programs = {
+    zsh.oh-my-zsh.enable = false;
+    oh-my-posh.enable = false;
+    git.signing.key = "8C07A97FC369A5F4FCFAC6F1989246B0B9904782";
+  };
   services.gpg-agent.sshKeys = [ "B5BE9A6227DB43612DCA51604EF35ABB0FD50B27" ];
   home.stateVersion = "25.05";
 }
