112822ec5dcf16cfb35796a831b21cd871d61dba
jtompkin <jtompkin-dev@pm.me>
2025-11-06 13:01:58 -0500

    remove extra stuff

--- modules/home-manager/wunkus/presets/programs/zsh-module.nix
+++ modules/home-manager/wunkus/presets/programs/zsh-module.nix
@@ -5,7 +5,7 @@
 }:
 let
   cfg = config.wunkus.presets.programs.zsh;
-  inherit (lib) mkDefault mkOverride;
+  inherit (lib) mkDefault;
 in
 {
   options.wunkus.presets.programs.zsh.enable = lib.mkEnableOption "Zsh preset configuration";
@@ -15,11 +15,6 @@ in
         enable = mkDefault true;
         dotDir = "${config.xdg.configHome}/zsh";
         shellAliases = {
-          # l = mkOverride 1001 "ls -lAhpv";
-          # ls = mkOverride 1001 "ls --color=tty --group-directories-first";
-          # la = mkOverride 1001 "ls -lahpv";
-          # ll = mkOverride 1001 "ls -lhpv";
-          # info = mkDefault "info --vi-keys";
           ssh-vm = mkDefault "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null";
           scp-vm = mkDefault "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null";
           rebuild = mkDefault "nixos-rebuild --flake 'github:jtompkin/dotfiles/main' --use-remote-sudo switch";
