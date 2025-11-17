5ecb782971f26a543f7d5bbe4342a021bf7215e5
jtompkin <jtompkin-dev@pm.me>
2025-11-17 13:25:26 -0500

    chore(home-modules): use Carbonfox kitty theme available now in nixpkgs

--- modules/home-manager/wunkus/presets/programs/kitty-module.nix
+++ modules/home-manager/wunkus/presets/programs/kitty-module.nix
@@ -23,18 +23,7 @@ in
       settings = {
         enable_audio_bell = "no";
       };
-      # TODO: Replace with `themeFile = "Carbonfox"` once pkgs.kitty-themes includes it
-      extraConfig = lib.readFile "${
-        pkgs.kitty-themes.overrideAttrs {
-          version = "2025-10-23";
-          src = pkgs.fetchFromGitHub {
-            owner = "kovidgoyal";
-            repo = "kitty-themes";
-            rev = "6af4bcd7244a20ce4a0244c9128003473b97f319";
-            hash = "sha256-oxNdwv5q3aEC6kCEZzZawrIYq0gYSVMjB4xVPb5WiEE=";
-          };
-        }
-      }/share/kitty-themes/themes/Carbonfox.conf";
+      themeFile = "Carbonfox";
     };
   };
 }
