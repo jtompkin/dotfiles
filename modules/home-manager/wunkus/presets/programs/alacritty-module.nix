37df956a7fa59a34c11eadf023a80990972f9248
jtompkin <jtompkin-dev@pm.me>
2025-11-23 11:36:18 -0500

    fix(mod alacritty): clean module and remove kitty configuration

--- modules/home-manager/wunkus/presets/programs/alacritty-module.nix
+++ modules/home-manager/wunkus/presets/programs/alacritty-module.nix
@@ -5,16 +5,13 @@
   ...
 }:
 let
-  inherit (lib) mkDefault;
   cfg = config.wunkus.presets.programs.alacritty;
+  inherit (lib) mkDefault;
 in
 {
   options.wunkus.presets.programs.alacritty.enable = lib.mkEnableOption "alacritty preset config";
   config = lib.mkIf cfg.enable {
-    home.packages = [
-      pkgs.nerd-fonts.meslo-lg
-      pkgs.nerd-fonts.iosevka
-    ];
+    home.packages = [ pkgs.nerd-fonts.iosevka ];
     programs = {
       alacritty = {
         enable = mkDefault true;
@@ -32,14 +29,5 @@ in
       };
     };
     fonts.fontconfig.enable = mkDefault true;
-    xdg.configFile."kitty/kitty.conf" = {
-      enable = !config.wunkus.presets.programs.kitty.enable;
-      text = ''
-        font_family MesloLGS Nerd Font
-        font_size 18.0
-        foreground #f2f4f8
-        background #161616
-      '';
-    };
   };
 }
