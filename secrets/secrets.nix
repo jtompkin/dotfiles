009d016ba5d650f2ae59e1453866d7bcf931110d
jtompkin <jtompkin-dev@pm.me>
2025-11-23 14:52:38 -0500

    refactor(secrets): clean ssh key definitions and remove extra file entries

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,23 +1,26 @@
 let
-  josh_franken = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINbmsUpHamS9g+sfhNhsBZ0DU5dO+7XpYMg+oPrMgeot";
-  deck_steamdeck = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILt5cxf21yGBEsSpqHIY/uzZwliBx4itTh9tqbBzXa1c";
-  users = [
-    josh_franken
-    deck_steamdeck
-  ];
-  franken = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDNHi5giEnLrSrWzuqg9G+lGaUfUq/1TbA1TOzC8Nzs+";
-  systems = [
-    franken
-  ];
+  systems = {
+    steamdeck = {
+      system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFhIQQ2UsFhweDONaAGeiLDeymYsxBnO5Uut3KSPhcBn";
+      deck = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILt5cxf21yGBEsSpqHIY/uzZwliBx4itTh9tqbBzXa1c";
+    };
+    franken = {
+      system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDNHi5giEnLrSrWzuqg9G+lGaUfUq/1TbA1TOzC8Nzs+";
+      josh = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINbmsUpHamS9g+sfhNhsBZ0DU5dO+7XpYMg+oPrMgeot";
+    };
+  };
+  allSystems = map (builtins.getAttr "system") (
+    builtins.filter (builtins.hasAttr "system") (builtins.attrValues systems)
+  );
+  allUsers = builtins.concatLists (
+    map builtins.attrValues (
+      map (system: builtins.removeAttrs system [ "system" ]) (builtins.attrValues systems)
+    )
+  );
 in
 {
   "pypi-token.age" = {
-    publicKeys = [
-      josh_franken
-      deck_steamdeck
-    ];
+    publicKeys = allUsers;
     armor = true;
   };
-  # "spotify-client-id-01.age".publicKeys = [ josh_ixodes ];
-  # "spotify-secret-01.age".publicKeys = [ josh_ixodes ];
 }
