91a5baae64ef0565d8ce3c34187a1cceb096a98e
jtompkin <jtompkin-dev@pm.me>
2026-03-04 10:29:47 -0500

    add backgrounds list to stylix module

--- modules/home-manager/wunkus/presets/themes/stylish-module.nix
+++ modules/home-manager/wunkus/presets/themes/stylish-module.nix
@@ -11,16 +11,32 @@ in
 {
   options.wunkus.presets.themes.stylish = {
     enable = lib.mkEnableOption "theming using stylix";
+    backgrounds = lib.mkOption {
+      type = lib.types.attrsOf lib.types.package;
+      default = {
+        mountain-peak-nighttime = pkgs.fetchurl {
+          name = "mountain_peak_nighttime.jpg";
+          url = "https://unsplash.com/photos/a0TJ3hy-UD8/download?force=true";
+          hash = "sha256-IxglIxt/7FM21GE5ntX0j+CdjRKAW7S3uAkARS46q10=";
+        };
+        space-shuttle-launch = pkgs.fetchurl {
+          name = "space_shuttle_columbia_launch.jpg";
+          url = "https://www.flickr.com/photo_download.gne?id=18307879358&secret=551f450ed5&size=o&source=photoPageEngagement";
+          hash = "sha256-R8k/6kG+aXxmGTdBUz+i88F1Hczk1XaqnMGlcgCGpM0=";
+        };
+        hubble-eye-nebula = pkgs.fetchurl {
+          name = "hubbele_eye_nebula.jpg";
+          url = "https://www.flickr.com/photo_download.gne?id=9467318602&secret=4ec6ae7bb4&size=o&source=photoPageEngagement";
+          hash = "sha256-28vjt4KEAZDR3f8YsoJikNRBuDwmZAAoutBP7gxX88s=";
+        };
+      };
+    };
   };
   config = lib.mkIf cfg.enable {
     stylix = {
       enable = mkDefault true;
       base16Scheme = mkDefault "${pkgs.vimPlugins.nightfox-nvim}/extra/carbonfox/base16.yaml";
-      image = pkgs.fetchurl {
-        name = "mountain_peak_nighttime.jpg";
-        url = "https://unsplash.com/photos/a0TJ3hy-UD8/download?force=true&w=2400";
-        hash = "sha256-F3/wWrIGnSPARNaQ0nB6y+TgvTlW+Dh6KH1ak5BuBe8=";
-      };
+      image = mkDefault cfg.backgrounds.mountain-peak-nighttime;
       fonts = {
         monospace = {
           name = mkDefault "Iosevka Nerd Font";
